5ebe167ffb5f39eb7b569a7674926c6da83db12f
Random_Dracolich <45239440+BlackrazorS2@users.noreply.github.com>
2022-10-25 16:20:31 -0400

    Create Updater.sh

---
+++ Updater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Updates the spreadsheet with the latest information and pushes backups and spreadsheet to github
+# The spreadsheet is going to go in both the data backup repository and the main repository
+
+python3 parser.py
+
+# Force pull repository
+git fetch
+git pull
+git add copied_logs/*
+git commit -m "Backups" copied_logs/*
+git commit -m "Updated Spreadsheet" Parsed_Spreadsheets/STATS_LATEST.csv
+git push
+
+cd ../HACS200-Research/Parsing
+git fetch
+git pull
+cp /home/students/HACS200-Research-Data-Backup/Parsed_Spreadsheets/STATS_LATEST.csv ./
+git add STATS_LATEST.csv
+git commit -m "Updated Spreadsheet" STATS_LATEST.csv
+git push
